232bb22c56dc09ba52f0004111ef22b17f3b4aaf
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2022-03-22 17:58:55 +0200

    finish chat order problem (Accepted)

--- sheet 6/2. Chat order.cpp
+++ sheet 6/2. Chat order.cpp
@@ -3,17 +3,32 @@ using namespace std;
 int main() {
 	// n is the number of messages
 	int n; cin >> n;
-	list<string> names;
-	while (n--) {
+
+	// this deque will be used to store all the names.
+	deque<string> names;
+		
+	// this set will be used to check if the name printed before or for the first time.
+	set<string> printer;
+
+	// read the names.
+	for (int i = 0; i < n; i++) {
 		string name; cin >> name;
-		auto itList = find(names.begin(), names.end(), name);
-		if (itList == names.end())
-			names.push_front(name);
-		else if (itList != names.end()) {
-			names.erase(itList);
-			names.push_front(name);
-		}
+		// add to the beginning of the data for easy printing.
+		names.push_front(name);
 	}
 
-	for (auto name : names) cout << name << endl;
+	for (auto name : names) {
+
+		// search in printer set (binary search) if we print this name before.
+		auto finder = printer.find(name);
+
+		// if we print it before we pass it and go to the next one.
+		if (finder != printer.end())
+			continue;
+
+		// if we didn't print it before we print it and add it to the printer set to avoid printing it again.
+		cout << name << endl;
+		printer.insert(name);
+
+	}
 }
