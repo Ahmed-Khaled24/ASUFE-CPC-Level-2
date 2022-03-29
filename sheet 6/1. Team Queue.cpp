132c6f0f581cb137918cf28e60465c2c4fc504a8
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2022-03-29 19:25:25 +0200

    finish incompleted team queue problem (Accepted)

--- sheet 6/1. Team Queue.cpp
+++ sheet 6/1. Team Queue.cpp
@@ -1,9 +1,12 @@
 #include<bits/stdc++.h>
+#include<unordered_map>
 using namespace std;
 
-void fillTheMap(map<int, int> &theMap, int numberOfTeams) {
+
+// o(n)
+void fillTheMap(unordered_map<int, int> &theMap, int numberOfTeams) {
 	// this will be used to identify each team members in the map
-	static int teamIdentifier = 1;
+	int teamIdentifier = 1;
 
 	// fill the map with teams members
 	for (int i = 0; i < numberOfTeams; i++) {
@@ -19,17 +22,28 @@ void fillTheMap(map<int, int> &theMap, int numberOfTeams) {
 	}
 }
 
-void DEQUEUE(list<int> &theQueue) {
-	int topElement = *theQueue.rbegin();
-	cout << topElement << endl;
-	theQueue.pop_back();
-}
 
-int getTheTeam(map<int, int>& theMap, int theElement) {
+// o(1) because the unordered map is a hash table
+int getTheTeam(unordered_map<int, int>& theMap, int theElement) {
 	auto it = theMap.begin();
 	it = theMap.find(theElement);
 	return it->second;
 }
+
+
+// o(1)
+void DEQUEUE(list<int>& theQueue, unordered_map<int, int>& theMap, vector<int>& counter, vector<list<int>::iterator>& positions) {
+	int topElement = theQueue.back();
+	int topElementTeam = getTheTeam(theMap, topElement);
+	cout << topElement << endl;
+	theQueue.pop_back();
+
+	// handle the positions arrya and counter array
+	counter[topElementTeam]--;
+	if (counter[topElementTeam] == 0)
+		positions[topElementTeam] = theQueue.end();
+}
+
 
 int main() {
 	int scenarioCounter = 1;
@@ -41,13 +55,24 @@ int main() {
 		if (t == 0) break;
 
 		// this map holds every member as a value and its team as a key
-		map<int, int> allMembers;
+		unordered_map<int, int> allMembers;
 		fillTheMap(allMembers, t);
+
+		//------------------------------------------------------------------------------
 
 		// handle the queries on the queue
 		list<int> queue;
 		string command = "";
 		cout << "Scenario #" << scenarioCounter << endl;
+
+		// array to keep track of the positoin of each team
+		vector<list<int>::iterator> positions(t + 1);
+		for (int i = 0; i < t + 1; i++) positions[i] = queue.end();
+
+
+		// array to keep track of each number of elements of the same team exist in the queue
+		vector<int> counter(t + 1, 0);
+
 
 		while (true) {
 
@@ -58,43 +83,25 @@ int main() {
 
 			// when we see dequeue command we print the first inserted element and remove it from the queue
 			else if (command == "DEQUEUE")
-				DEQUEUE(queue);
+				DEQUEUE(queue, allMembers, counter, positions);
 			
 			// when we see enqueue command we add the following element wrt the given instructions
 			else if (command == "ENQUEUE") {
 				int theElement; cin >> theElement;
-				int theElementTeam = -1;
+				int theElementTeam = getTheTeam(allMembers, theElement);
 
-				// if we insert the first element to the queue
-				if (queue.empty()) {
-					queue.push_front(theElement);
-					continue;
-				}
+				// insert the element at the position of the last one of its team 
+				auto insertPosition = positions[theElementTeam];
+				if (insertPosition == queue.end())  
+					positions[theElementTeam] = queue.insert(queue.begin(), theElement);
+				else 
+					positions[theElementTeam] = queue.insert(insertPosition, theElement);
 
-				// check the element team
-				theElementTeam = getTheTeam(allMembers, theElement);
+				counter[theElementTeam]++;
 				
-				// find the team for each member in the queue and compare with theElementTeam
-				bool foundTheTeam = false;
-				auto itList = queue.begin();
-				while(itList != queue.end()) {
-					int currentElement = *itList;
-					auto itMap = allMembers.find(currentElement);
-					int currentElementTeam = itMap->second;
-
-					if (theElementTeam == currentElementTeam) {
-						queue.insert(itList, theElement);
-						foundTheTeam = true;
-						break;
-					}
-
-					itList++;
-				}
-				// if there is no elements in the queue from the same team
-				if (!foundTheTeam)
-					queue.push_front(theElement);
 			}				
 		}
 		scenarioCounter++;
+		cout << endl;
 	}
 }
